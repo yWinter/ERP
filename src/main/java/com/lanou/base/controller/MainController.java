1ecf33b69d4d14273d63de83d80ee9fd8213d165
LiJingG <1126607503@qq.com>
2017-12-05 17:23:53 +0800

    ERP项目初始模板

--- src/main/java/com/lanou/base/controller/MainController.java
+++ src/main/java/com/lanou/base/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+    //跳转到登录页面
     @RequestMapping(value = "/")
     public String loginPage(){
         return "login";
